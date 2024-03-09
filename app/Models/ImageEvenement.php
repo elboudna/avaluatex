ede02e7d60f204df5fe033b38913aa9406cca41d
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-09 11:21:49 -0500

    Maj

--- app/Models/ImageEvenement.php
+++ app/Models/ImageEvenement.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ImageEvenement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'position_x',
+        'position_y',
+        'image_url',
+        'evenement_id',
+    ];
 }
