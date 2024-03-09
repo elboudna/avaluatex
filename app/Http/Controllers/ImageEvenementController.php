331f90eb5d87bf751c9999b3d4f8948c9241e9a4
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-09 13:35:12 -0500

    Maj

--- app/Http/Controllers/ImageEvenementController.php
+++ app/Http/Controllers/ImageEvenementController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ImageEvenement;
+use App\Models\Evenement;
 
 class ImageEvenementController extends Controller
 {
-    //
 
     public function store(Request $request)
     {
@@ -16,14 +16,22 @@ class ImageEvenementController extends Controller
             'position_y' => 'required|numeric',
             'image_url' => 'required|string',
             'evenement_id' => 'required|numeric',
+            'numero_evenement' => 'required|numeric',
+            'game_id' => 'required|numeric',
         ]);
 
         // Création d'une nouvelle imageEvenement
         ImageEvenement::create($validatedData);
 
+        // update the icon of the evenement
+        $evenement = Evenement::find($request->evenement_id);
+        $evenement->icone = '0';
+        $evenement->save();
+
         // Redirection a la page game.blade en recuperant les données de la base de données
         return redirect('/game/go?id='.$request->game_id)->with('success', 'imageEvenement créé avec succès');
-
     }
+
+
 }
 
